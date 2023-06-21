c13483135246b9d4b329fb175c885e9ee8cbe82c
Backend CoderHouse <nachodallape2@gmail.com>
2023-06-21 08:34:09 -0300

    Redvivir 21-6

--- src/components/principal/Principal.jsx
+++ src/components/principal/Principal.jsx
@@ -17,14 +17,34 @@ const Principal = () => {
             </div>
             <h3 className='centrado' style={{ padding: '20px' }}> Nuestro Objetivo</h3>
 
-            <div className=' container' style={{ display: 'flex', justifyContent: 'center', padding: '2rem' }}>
-                <div style={{ textAlign: 'justify', paddingRight: '15rem', display:'flex',alignItems:'center' }} className='principal__parrafo'>
+            <div className=' container' style={{ display: 'flex', justifyContent: 'center', padding: '1rem', paddingTop: '0rem'}}>
+                <div style={{ textAlign: 'justify', paddingRight: '15rem', display:'flex',alignItems:'center', position:'relative', bottom:'3rem'  }} className='principal__parrafo'>
                     <p style={{ fontFamily: 'Roboto' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, possimus dicta. Possimus quis quasi velit veniam beatae perferendis expedita aliquam quo optio ea nostrum dicta neque quibusdam sequi maxime numquam voluptate accusantium error asperiores aspernatur, natus vel eveniet? Dignissimos incidunt error perspiciatis a nihil aut assumenda nesciunt minus. Blanditiis mollitia repellendus quidem ipsa nostrum iusto autem, consequuntur exercitationem et magni ut labore velit architecto rem placeat ad reiciendis eveniet minima deserunt nobis cumque rerum quod totam. Error aperiam, ratione nesciunt ea sequi eius cum rem nobis eaque perspiciatis eum voluptas eos, vero officiis libero quae, asperiores minus laboriosam sit incidunt!</p>
                 </div>
                 <div>
                     <img src="https://i.ibb.co/0QN97rL/b4c93276125df370682cef2667e47323.jpg" alt="" style={{ width: '30rem' }} />
                 </div>
 
+            </div>
+            <h3 className='centrado' style={{paddingTop:'2rem'}}>¡Nuestros Servicios!</h3>
+            <div className='container' style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '2rem', paddingBottom: '4rem' }}>
+                <div>
+                    <h5 className='centrado' style={{paddingTop:'2rem'}}>Entrevistas Presenciales</h5>
+                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:'2rem', paddingBottom:'2rem'}}>
+                        <img src="https://i.ibb.co/bQ2kK86/a2e0c177a3498cef60fbfc859860d238-1.jpg" alt="" style={{ width: '15rem' }} />
+                        <br />
+                        <p style={{ textAlign: 'justify', paddingTop:'2rem' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus accusantium modi non omnis dicta consectetur autem cum neque voluptatibus. Debitis impedit quibusdam minus, consectetur dicta porro. Reiciendis, excepturi doloremque!</p>
+                    </div>
+                </div>
+                <div>
+                    <h5 className='centrado' style={{paddingTop:'2rem'}}>Entrevistas On-line</h5>
+                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:'2rem', paddingBottom:'2rem'}}>
+                        <img src="https://i.ibb.co/XXyjMqT/60a39d611fbc5cb64ede206012ef3548.jpg" alt="" style={{ width: '15rem' }} />
+                        <br />
+                        <p style={{ textAlign: 'justify', paddingTop:'2rem' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus accusantium modi non omnis dicta consectetur autem cum neque voluptatibus. Debitis impedit quibusdam minus, consectetur dicta porro. Reiciendis, excepturi doloremque!</p>
+                    </div>
+
+                </div>
             </div>
             <div className='container' style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column', alignItems: 'center', gap: '3rem', paddingBottom: '10rem' }}>
                 <h4>Integrantes</h4>
@@ -33,26 +53,6 @@ const Principal = () => {
                 </div>
                 <img src="https://i.ibb.co/x8YhyPN/7754bfe359ee45721afe69093259ce34.jpg" alt="" style={{ width: '30rem' }} />
 
-            </div>
-            <h3 className='centrado'>¡Nuestros Servicios!</h3>
-            <div className='container' style={{ display: 'flex', justifyContent: 'space-around', paddingTop: '2rem', paddingBottom: '5rem' }}>
-                <div>
-                    <h5 className='centrado'>Entrevistas Presenciales</h5>
-                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:'2rem', paddingBottom:'2rem'}}>
-                        <img src="https://i.ibb.co/bQ2kK86/a2e0c177a3498cef60fbfc859860d238-1.jpg" alt="" style={{ width: '15rem' }} />
-                        <br />
-                        <p style={{ textAlign: 'justify' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus accusantium modi non omnis dicta consectetur autem cum neque voluptatibus. Debitis impedit quibusdam minus, consectetur dicta porro. Reiciendis, excepturi doloremque!</p>
-                    </div>
-                </div>
-                <div>
-                    <h5 className='centrado'>Entrevistas On-line</h5>
-                    <div style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center', padding:'2rem', paddingBottom:'2rem'}}>
-                        <img src="https://i.ibb.co/XXyjMqT/60a39d611fbc5cb64ede206012ef3548.jpg" alt="" style={{ width: '15rem' }} />
-                        <br />
-                        <p style={{ textAlign: 'justify' }}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis possimus accusantium modi non omnis dicta consectetur autem cum neque voluptatibus. Debitis impedit quibusdam minus, consectetur dicta porro. Reiciendis, excepturi doloremque!</p>
-                    </div>
-
-                </div>
             </div>
         </>
     )
