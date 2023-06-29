440f6dd4c0266927515ab3f178614de598c12dda
Sabrina <marchiori.sabrina@hotmail.com>
2023-06-28 22:25:24 -0300

    css del entrevistas personalizadas

--- src/components/entrevistasPersonalizadas/EntrevistasPersonalizadas.jsx
+++ src/components/entrevistasPersonalizadas/EntrevistasPersonalizadas.jsx
@@ -5,20 +5,20 @@ const EntrevistasPersonalizadas = () => {
   return (
     <main>
       <article >
-        <div  style={{ display: 'flex', justifyContent: 'center', alignItems: 'self-start', gap: '2rem', paddingBottom: '3rem', flexDirection:'column', alignItems:'center' }}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'self-start', gap: '2rem', paddingBottom: '3rem', flexDirection: 'column', alignItems: 'center' }}>
           <h3 className='article__escucha ' > Escucha Activa</h3>
 
           <div style={{ textAlign: 'justify', paddingRight: '15rem', display: 'flex', alignItems: 'column', position: 'inherit', bottom: '3rem' }} className='text_escucha container'>
 
             <p style={{ fontFamily: 'Roboto' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, possimus dicta. Possimus quis quasi velit veniam beatae perferendis expedita aliquam quo optio ea nostrum dicta neque quibusdam sequi maxime numquam voluptate accusantium error asperiores aspernatur, natus vel eveniet? Dignissimos incidunt error perspiciatis a nihil aut assumenda nesciunt minus. Blanditiis mollitia repellendus quidem ipsa nostrum iusto autem, consequuntur exercitationem et magni ut labore velit architecto rem placeat ad reiciendis eveniet minima deserunt nobis cumque rerum quod totam. Error aperiam, ratione nesciunt ea sequi eius cum rem nobis eaque perspiciatis eum voluptas eos, vero officiis libero quae, asperiores minus laboriosam sit incidunt!</p>
-    <div>
-      
-          <div className=''>
-            <img src='https://i.ibb.co/k3YZxCx/entrevista-presencial-1.jpg' alt='escucha_activa' style={{ width: '25rem', borderRadius: '10px' }} />
+            <div>
 
+              <div className=''>
+                <img src='https://i.ibb.co/k3YZxCx/entrevista-presencial-1.jpg' alt='escucha_activa' style={{ width: '25rem', borderRadius: '10px' }} />
+
+              </div>
+            </div>
           </div>
-          </div>
-    </div>
         </div>
       </article>
       <hr />
