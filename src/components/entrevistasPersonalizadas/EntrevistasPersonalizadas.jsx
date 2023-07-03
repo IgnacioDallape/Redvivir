78a3a28698ce3155458bc7db06fcc32a431a2cab
Backend CoderHouse <nachodallape2@gmail.com>
2023-07-03 19:07:25 -0300

    arreglo enm entrevistas perzonalizadas

--- src/components/entrevistasPersonalizadas/EntrevistasPersonalizadas.jsx
+++ src/components/entrevistasPersonalizadas/EntrevistasPersonalizadas.jsx
@@ -5,30 +5,77 @@ const EntrevistasPersonalizadas = () => {
   return (
     <main>
       <article >
-        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'self-start', gap: '2rem', paddingBottom: '3rem', flexDirection: 'column', alignItems: 'center' }}>
+        <div className="container pt-3" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', gap: '2rem', paddingBottom: '3rem', flexDirection: 'column', alignItems: 'center', flexDirection: "column" }}>
 
-          <h3 className='article__escucha ' > Escucha Activa</h3>
-
-          <div className='text_escucha container' style={{ textAlign: 'justify', paddingRight: '15rem', display: 'flex', alignItems: 'column', bottom: '3rem' }} >
-
-            <p style={{ fontFamily: 'Roboto' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, possimus dicta. Possimus quis quasi velit veniam beatae perferendis expedita aliquam quo optio ea nostrum dicta neque quibusdam sequi maxime numquam voluptate accusantium error asperiores aspernatur, natus vel eveniet? Dignissimos incidunt error perspiciatis a nihil aut assumenda nesciunt minus. Blanditiis mollitia repellendus quidem ipsa nostrum iusto autem, consequuntur exercitationem et magni ut labore velit architecto rem placeat ad reiciendis eveniet minima deserunt nobis cumque rerum quod totam. Error aperiam, ratione nesciunt ea sequi eius cum rem nobis eaque perspiciatis eum voluptas eos, vero officiis libero quae, asperiores minus laboriosam sit incidunt!</p>
-            <div>
-
-              <div className='article_activa'>
-                <img src='https://i.ibb.co/k3YZxCx/entrevista-presencial-1.jpg' alt='escucha_activa' style={{ width: '25rem', borderRadius: '10px' }} />
-
-              </div>
+          <div className='text_escucha container' style={{ textAlign: 'justify', paddingRight: '15rem', display: 'flex', alignItems: 'column', bottom: '3rem', padding: '1rem' }} >
+            <div style={{ padding: '2rem', paddingRight: '5rem' }}>
+              <h3 className='d-flex justify-content-center pt-1'>
+                Escucha Activa
+              </h3>
+              <p style={{ fontFamily: 'Roboto' }}>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa, possimus dicta. Possimus quis quasi velit veniam beatae perferendis expedita aliquam quo optio ea nostrum dicta neque quibusdam sequi maxime numquam voluptate accusantium error asperiores aspernatur, natus vel eveniet? Dignissimos incidunt error perspiciatis a nihil aut assumenda nesciunt minus. Blanditiis mollitia repellendus quidem ipsa nostrum iusto autem, consequuntur exercitationem et magni ut labore velit architecto rem placeat ad reiciendis eveniet minima deserunt nobis cumque rerum quod totam. Error aperiam, ratione nesciunt ea sequi eius cum rem nobis eaque perspiciatis eum voluptas eos, vero officiis    libero quae, asperiores minus laboriosam sit incidunt!</p>
+              <h4 className="d-flex justify-content-center pt-5">¿Cómo funciona?</h4>
+              <p className="pt-4">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Ea, alias! Id esse delectus dolorem a optio ipsum sunt eveniet recusandae consequatur praesentium voluptatibus facilis fuga explicabo, vero doloribus sequi nulla iste dolorum illum deleniti? Consequatur animi, ratione hic, sapiente sunt ipsam veritatis beatae assumenda iste, aperiam dicta harum. Quas incidunt illo impedit in ducimus. Quibusdam nulla suscipit magnam enim earum corrupti ratione, odit minima voluptatem, aliquid sint vitae libero reiciendis totam rerum alias voluptas dignissimos. Nobis fuga rem sunt fugit ipsa, nisi qui omnis? Deleniti molestiae aspernatur maiores iusto perferendis sequi non blanditiis tempore similique repudiandae. Iusto, magni esse. Maxime pariatur in aliquid. Ea, fuga sapiente vero ipsa alias quia reprehenderit eveniet, voluptatem aspernatur quibusdam odio omnis, consequatur ipsam iste!
+              </p>
+            </div>
+            <div className='article_activa pt-5'>
+              <img src='https://i.ibb.co/k3YZxCx/entrevista-presencial-1.jpg' alt='escucha_activa' style={{ width: '23rem', borderRadius: '10px' }} />
             </div>
           </div>
         </div>
       </article>
-      <hr />
 
-      <section className='textBox' style={{ backgroundColor: 'lightgray' }}>
-        <div className='img'>
+      <section className='textBox pb-5'>
+        <div className="d-flex flex-column align-items-center pt-5" >
+          <form action="" className="p-5" style={{ backgroundColor: 'lightgray', borderRadius: '1rem' }}>
+            <div className="form-group">
+              <label htmlFor="name">Nombre</label>
+              <input type="text" name="name" id="name" className="form-control" />
+            </div>
 
+            <div className="form-group">
+              <label htmlFor="lastname">Apellido</label>
+              <input type="text" name="lastname" id="lastname" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label>Sexo</label>
+              <div style={{display:"flex", gap:'10px'}}>
+
+              <div>
+                <input type="radio" id="hombre" name="gender" value="hombre" />
+                <label htmlFor="hombre">Hombre</label>
+              </div>
+              <div>
+                <input type="radio" id="mujer" name="gender" value="mujer" />
+                <label htmlFor="mujer">Mujer</label>
+              </div>
+              <div>
+                <input type="radio" id="nodir" name="gender" value="nodir" />
+                <label htmlFor="nodir">Prefiero no decirlo</label>
+              </div>
+              </div>
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="email">E-mail</label>
+              <input type="email" name="email" id="email" className="form-control" />
+            </div>
+
+            <div className="form-group">
+              <label htmlFor="celphone">Celular</label>
+              <input type="number" name="celphone" id="celphone" className="form-control" />
+            </div>
+
+            <div className="form-group pt-3 pb-3">
+              <button type="submit" className="btn btn-primary">Enviar</button>
+            </div>
+          </form>
         </div>
-        <div class='form_box'>
+      </section>
+
+
+      {/* <div class='form_box'>
           <form action="/">
             <h1>Datos para la Consulta</h1>
             <hr />
@@ -59,8 +106,7 @@ const EntrevistasPersonalizadas = () => {
               </div>
             </div>
           </form>
-        </div>
-      </section>
+        </div> */}
     </main>
   )
 
